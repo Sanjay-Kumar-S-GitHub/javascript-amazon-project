37ab16539d382bf527a0925868a83d4420f89fb2
Sanjay-Kumar-S-GitHub <sanjay.k.sundararajan@gmail.com>
2025-01-01 18:38:31 +0530

    save cart in localStorage

--- javascript-amazon-project/data/cart.js
+++ javascript-amazon-project/data/cart.js
@@ -1,14 +1,21 @@
-export let cart=[
-    {
-    productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-    quantity: 2
-},{
-    productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
-    quantity: 1
+export let cart= JSON.parse(localStorage.getItem('cart'));  //this will give cart=NULL as we will not have cart in local storage if use website for first time
+
+if(!cart){
+    cart = [
+        // {
+        // productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+        // quantity: 2
+        // },{
+        // productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+        // quantity: 1
+        // }
+    ];
 }
-];
 
 
+function saveToStorage(){
+    localStorage.setItem('cart', JSON.stringify(cart));
+}
 
 export function addTocart(productId){
     let matchingItem;
@@ -28,6 +35,8 @@ export function addTocart(productId){
             quantity: 1,
         });
     }
+
+    saveToStorage();
 }
 
 
@@ -41,4 +50,6 @@ export function removeFromCart(productId){
     });
     
     cart = newCart;
+
+    saveToStorage();
 }
